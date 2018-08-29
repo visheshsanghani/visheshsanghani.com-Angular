5a0e1c5c7e1f3defb80903191d6737a33a7b0be6
visheshsanghani <vishesh.sanghani95@gmail.com>
2018-08-29 17:16:56 +0530

    First git

---
+++ que.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function(){
+  jQuery('.progress-bar').each(function() {
+    jQuery(this).find('.progress-content').animate({
+      width:jQuery(this).attr('data-percentage')
+    },2000);
+
+    jQuery(this).find('.progress-number-mark').animate(
+      {left:jQuery(this).attr('data-percentage')},
+      {
+       duration: 2000,
+       step: function(now, fx) {
+         var data = Math.round(now);
+         jQuery(this).find('.percent').html(data + '%');
+       }
+    });
+  });
+
+});
